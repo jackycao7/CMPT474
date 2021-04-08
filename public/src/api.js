8250e25086cf532ce37dc8afc239335dccdc5a5e
aeginyan <51391875+aeginyan@users.noreply.github.com>
2021-04-08 02:53:47 -0700

    Delete api.js

--- public/src/api.js
+++
@@ -1,7 +0,0 @@
-let api = {
-    gateway: "https://o8khyqjc1j.execute-api.us-east-1.amazonaws.com/dev/",
-    albumBucketName: "petfindrbucket",
-    bucketRegion: "us-east-1",
-    IdentityPoolId: "us-east-1:ed4db688-f223-4b71-980e-132be527a300",
-}
-export default api;
