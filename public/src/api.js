800716c72fdc0a59e1417de8c3a78b2a88272198
yew <39639471+yewinston@users.noreply.github.com>
2021-04-08 00:22:23 -0700

    no api.js pls

--- public/src/api.js
+++
@@ -1,8 +0,0 @@
-let api = {
-    gateway: "https://1s1p98yyu5.execute-api.us-east-1.amazonaws.com/dev/",
-    albumBucketName: "mypetfindrbucket",
-    bucketRegion: "us-east-1",
-    IdentityPoolId: "us-east-1:9d8c2e1f-692c-49d5-bf53-b999eed86683",
-}
-
-export default api;
