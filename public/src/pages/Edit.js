3feaa09ede2673f1366bc4badbd88be0530ced13
Jacky <jackyftw2@gmail.com>
2021-04-08 20:13:02 -0700

    Fix api url for DELETE

--- public/src/pages/Edit.js
+++ public/src/pages/Edit.js
@@ -91,7 +91,7 @@ export default function Edit() {
   async function deletePosting(uuid){
     let api_url = api.gateway + "/postings/delete?uuid=" + uuid;
 
-    await fetch(api.gateway, {
+    await fetch(api_url, {
         method: 'DELETE', // *GET, POST, PUT, DELETE, etc.
         mode: 'cors', // no-cors, *cors, same-origin
         cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
